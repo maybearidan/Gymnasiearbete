08afa10a9e3b885a34638e4fca65480f7a07811a
maybearidan <96298432+maybearidan@users.noreply.github.com>
2022-09-23 02:15:06 +0200

    Lade till navigering för mobiler

---
+++ scrpit/jumpquickheader.js
@@ -0,0 +1,17 @@
+function showNav() {
+    var x = document.getElementById("quickjumpmenuwithheader");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+function showMenu() {
+    var x = document.getElementById("homemenuwithheader");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
