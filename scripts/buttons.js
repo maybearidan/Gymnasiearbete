ab572ef9043ccccba5323c0d2c55666848599436
maybearidan <96298432+maybearidan@users.noreply.github.com>
2022-12-05 13:57:36 +0100

    add holder

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,3 +1,15 @@
+window.onload = function WindowLoad(event) {
+  const date = new Date()
+  const saveDate = Date.parse(localStorage.getItem("Agree"))
+  var dif = date.getTime() - saveDate
+
+  console.log(dif)
+  // Don't hide since 15 minutes of agreement
+  if(dif < 15*60*1000) {
+    macosUnhide(0)
+  }
+}
+
 function showNav() {
   var meny = document.getElementById("homemenuwithheader");
   var nav = document.getElementById("quickjumpmenuwithheader");
@@ -22,7 +34,11 @@ function showMenu() {
     }
 }
 
-function macosUnhide() {
+function macosUnhide(doreset) {
+  const date = new Date()
+  if(doreset)
+    localStorage.setItem("Agree", date)
+
   var text = document.getElementById("mactext");
   var notice = document.getElementById("alert");
     text.style.display = "block";
